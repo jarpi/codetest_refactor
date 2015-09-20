ffced8cc5a1a2fb11cc453ac48025471c334d9a8
jarpi <josep.arpi@gmail.com>
2015-09-20 19:29:24 +0200

    - Refactored SpaghettiBrain

--- brain.js
+++ brain.js
@@ -1,82 +1,43 @@
 function SpaghettiBrain(text) {
-    this.characters = text.split('');
-    var brain = this;
+    var characters = text.split('');
     var thoughts = [];
 
     this.think = function() {
-        var item = parseInt(Math.random() * this.characters.length, 10);
-        var thought = brain.characters[item];
+        
+        var item = parseInt(Math.random() * characters.length, 10);
+        var thought = characters[item];
 
-        var exists = false;
-        for (var j = 0; j < thoughts.length; j++) {
-            if (thoughts[j] === thought) {
-                exists = true;
-            }
-        }
-
-        if (exists) {
-            return 'Uhm... I already said that : ' + thought;
-        } else {
-            thoughts.push(thought);
-            return thought;
-        }
+        (existsThought(thought)?
+            thought = 'Uhm... I already said that : ' + thought : 
+            addThought(thought)) 
+        return thought; 
     };
 
     this.talk = function() {
         console.log('Hi hero, i think that: ' + this.think());
     };
+
+    function addThought(th) { thoughts.push(th) }; 
+    function existsThought(th) { return (thoughts.indexOf(th)>-1) }; 
 }
 
-function spaghettiName(amount) {
-
-    var things = [];
+function SpaghettiName(amount) {
     var results = [];
-
     for (var i = 0; i < amount; i++) {
-        things.push({
-            i: i,
-            value: Math.random() * i
-        });
+        results.push( Math.floor(Math.random() * 25) );
     }
 
-    for (var i = 0; i < amount; i++) {
-        if (things[i].value > amount / 2) {
-            results.push(things[i]);
+    var myName = results.reduce(
+        function(p,c) {
+            return p+= String.fromCharCode( 97 + parseInt(c,10) );
         }
-    }
-
-
-    function doSomeWork(value, i, cb) {
-        var a = Math.pow(value, 3);
-        a = a + i;
-        a = a / amount;
-
-        return cb(a, i);
-    }
-
-    function callback(value, i) {
-        return value * things[i].value;
-    }
-
-    var theFinalResult = '';
-
-    for (var i = 0; i < results.length; i++) {
-        theFinalResult += doSomeWork(results[i].value, i, callback);
-    }
-
-    var numbers = theFinalResult.split('');
-    var myName = '';
-
-    numbers.forEach(function(number) {
-        myName += String.fromCharCode(97 + number);
-    });
-
+        ,'');
     return myName;
 }
 
-var name = spaghettiName(30);
+var name = SpaghettiName(30);
 var brain = new SpaghettiBrain(name);
-
-for (var i = 0; i < 100; i++) {
+console.dir(brain); 
+for (var i = 0; i < name.length; i++) {
     brain.talk();
 }
